49c1f77ceb06b90d725afc6933422de7f823a3d2
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2023-09-11 18:40:04 +0300

    Update ps.php

--- ps.php
+++ ps.php
@@ -1,77 +1,4 @@
 <?php
-/*
-for($i=1;$i<=10;$i++){
-    echo $i . "\n";
-}
-
-#print odd no. less than 100
-
-for($i=1;$i<100;$i+=2){
-    echo $i;
-}
-echo "\n";
-
-
-for($i=1;$i<=10;$i++){
-    $x="7 * " . $i . " = " . 7*$i;
-    echo $x . "\n"; 
-}
-*/
-// $x = 0;
-// for($i=11;$i<=30;$i++){
-//     global $x;
-//     $x += $i;
-// }
-// echo $x;
-
-// $x = 0;
-// for($i=11;$i<=30;$i++){
-//     global $x;
-//     if($i%2 == 0){
-//         $x += $i; 
-//     }
-//     continue;
-// }
-// echo $x;
-
-
-
-// $arr = [2,3,-1,5,7,9,10,15,95];
-// // print_r ($arr);
-// $arr_len = count($arr);
-// echo $arr_len;
-
-// function sum($arr){
-// global $arr_len;
-// $x=0;
-// for($i=0;$i<$arr_len;$i++){
-//     $x += $arr[$i];
-// }
-// return $x;
-// }
-// echo sum($arr);
-// $max = 0;
-// $second_max = 0;
-// for($i=0;$i<$arr_len;$i++){
-//     if($max < $arr[$i]){
-//         $max = $arr[$i];
-//     }
-// }
-// for($i=0;$i<$arr_len;$i++){
-//     if($arr[$i]==$max){
-//         continue;
-//     }else if($second_max < $arr[$i]){
-//         $second_max = $arr[$i];
-//     }
-// }
-// echo $second_max;
-
-// $sorted = sort($arr);
-// print_r($arr);
-
-
-
-
 function fun($arr){    
     $element = 0;    
     $max_count = 0;
@@ -87,7 +14,7 @@ function fun($arr){
             $element = $arr[$i];
         }
     }
-}
+    }
 return $element;
 }
 $arr = [8,7,7,6,6,6,5]; //print the most repeated element
